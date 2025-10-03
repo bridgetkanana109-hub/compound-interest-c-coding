5e17c7164e5ac065489134d91ea423ce42bb1ba0
bridgetkanana109-hub <bridgetkanana109@gmail.com>
2025-10-03 12:55:49 +0300

    Add files via upload

---
+++ Loan_Application.c
@@ -0,0 +1,23 @@
+/*
+Name:Bridget Kanana
+Reg no:CT101/G/26558/25
+Description:loan application 
+*/
+int main()
+{
+    int age;
+	double income;
+	
+	printf("Enter the age:");
+	scanf("%d",&age);
+	
+	printf("Enter the income:");
+	scanf("%lf",&income);
+	
+	if(age>= 21 &&income>=21000)
+	{printf("congratulation, you qualify for a loan.\n");} else
+	{printf("unfortunately,we are unable to offer you a loan at this time.\n"
+	);}
+	
+	return 0;
+	}
