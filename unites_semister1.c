b8ed9f50ee357aaec9d03c29f5c791c7a6427589
bridgetkanana109-hub <bridgetkanana109@gmail.com>
2025-10-03 15:36:22 +0300

    Add files via upload

---
+++ unites_semister1.c
@@ -0,0 +1,19 @@
+/*
+Name:Bridget kanana
+Reg_No:CT101/G/26558/25
+Date:28/09/2025
+Description:unites_semister1
+*/
+#include<stdio.h>
+int main()
+{
+	printf("SPC 2101 Introduction to programming and problem solving\n");
+	printf("SPC 2102 Introduction to computer Architucture\n");
+	printf("SPH 2101 Physics\n");
+	printf("SPH 2107 Analoge electronics\n");
+	printf("SPM 2100 Discete Mathemactics\n");
+	printf("SPM 2104 Mathemactics for science\n");
+	printf("UCU 2105 Fundermental of ICT\n");
+	printf("UCU 2106 Communication and information literacy skills\n");
+
+	return 0;
